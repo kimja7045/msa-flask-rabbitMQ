416d8d83eac7a5c3433b01271e20d5ee57370842
jaehoonKim <temp7045@gmail.com>
2022-08-21 11:38:31 +0900

    feat(rabbitMQ): add mbs setup & test

---
+++ consumer.py
@@ -0,0 +1,27 @@
+import pika
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+params = pika.URLParameters(getenv('AMQP_URL'))
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='boss')
+
+
+def callback(ch, method, properties, body):
+    print('Received in boss')
+    print(body)
+
+
+channel.basic_consume(queue='boss', on_message_callback=callback)
+
+print('Started consuming at boss')
+
+channel.start_consuming()
+
+channel.close()
